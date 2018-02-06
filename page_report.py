f4a9f03270d5bfe420ae93ca8c2bb0eccdbad0cb
Tomasz Czepiel <Czep.tomasz@gmail.com>
2018-02-06 15:36:44 +0100

    solved problem with output

--- page_report.py
+++ page_report.py
@@ -5,18 +5,10 @@ import collections
 from urllib.parse import urlparse
 
 # REMARKS!!!
-# I've had a problem with "today.log > report.csv" syntax. Right now the script supports "today.log report.csv".
-# From what I know ">" is acted upon by shell, and duplicates the STDOUT stream (FD 1) to the second file.
-# This happens before the command even runs, so I can't find a way to preserve this syntax and have the output
-# file available in my script.
-
 # I am not 100% happy with this solution. There are too many conditionals and loops.
 # I feel it is a little overcomplicated.
 
 inFile = sys.argv[1]
-outFile = sys.argv[2]
-
-invalidLines = 0
 
 def regex(input):
     pattern = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
@@ -25,7 +17,7 @@ def regex(input):
         domain = '{url.netloc}{url.path}'.format(url=urlparse(input[match.start():match.end()])).rstrip('/')
         return domain
 
-with open(inFile, 'r+') as input,open(outFile,'w+') as out:
+with open(inFile, 'r+') as input:
     dict = {"invalid log lines":0}
     for line in input:
         if regex(line):
@@ -39,4 +31,4 @@ with open(inFile, 'r+') as input,open(outFile,'w+') as out:
     sorted = collections.OrderedDict(sorted(dict.items(), key=lambda t: t[1], reverse=True))
 
     for k, v in sorted.items():
-        out.write(str(k) + ':' + str(v) + '\n')
+        print(str(k) + ':' + str(v) + '\n')
